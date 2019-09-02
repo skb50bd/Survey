3b4f12270522bdc72ba5e41f1bea5c03c0dae795
Ashiful Arefin <arefin.ashiful@gmail.com>
2019-09-02 10:54:23 +0600

    Summary Response Only HTML

---
+++ Web/Pages/Responses/Summary.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Data;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Web.Pages.Responses
+{
+    public class SummaryModel : PageModel
+    {
+        public ResponseSummary ResponseSummary { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
